356fa8f8f62b6eb04f9c6f0860038c31658410a4
luciateng688-tech <luciateng688@gmail.com>
2025-11-25 14:26:29 +0800

    Update p3.cpp

--- src/p3.cpp
+++ src/p3.cpp
@@ -1,10 +1,28 @@
 // 題目 3：破解數學家的謎題 (Cracking the Mathematician's Puzzle) - 30分
 
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int main() {
-    
-    
+int func(int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        int x=i;
+        n=x+(n-x);
+    for (int j=0 ; j<(x/2) ; i++)
+      {
+        if(x%i>0 && (x-1)%i>0)
+        break;
+      }
+    return x ;
+    }
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    cout << func(n);
     return 0;
 }
