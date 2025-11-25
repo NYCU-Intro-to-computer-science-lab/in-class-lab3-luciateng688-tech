96d7ef0bc5fbcf1810ae1bc94ee565b572a4e982
luciateng688-tech <luciateng688@gmail.com>
2025-11-25 14:05:37 +0800

    Update p5.cpp

--- src/p5.cpp
+++ src/p5.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
-#include <cmath>
-
+#include <stdio.h>
 using namespace std;
 
+void river(int a,char from,char temp,char to)
+{
+    if (a==1)
+    {
+        cout << "Move disk 1 from "<<from << " to " << to << "\n";
+        return;
+    }
 
-int main() {
-    
+    river (a-1,from,to,temp); 
+    cout << "Move disk "<< a <<" from "<<from << " to " << to << "\n";
 
+    river (a-1,temp,from,to);
+}
+
+int main()
+{
+    int a;
+    while(cin >> a) 
+    {
+        river (a,'1','2','3');
+        cout << "\n";
+    }
     return 0;
 }
